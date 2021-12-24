66286341e035380c6ca14dfd2e93ab5b1bfa73f1
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-24 11:14:50 +0900

    create restaurant.spec.ts

---
+++ test/admin/restaurant.spec.ts
@@ -0,0 +1,24 @@
+import test from 'japa'
+import { RestaurantFactory } from 'database/factories/restaurant'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+const request = supertest(BASE_URL)
+
+test.group('Restaurant', () => {
+  test('restaurant get all', async () => {
+    await request.get('/api/v1/admin/restaurants').expect(200)
+  })
+  test('restaurant get single restaurant', async () => {
+    await request.get('/api/v1/admin/restaurants/1').expect(200)
+  })
+  test('create restaurant', async () => {
+    await request.post('/api/v1/admin/restaurants').expect(200)
+  })
+  test('update restaurant', async () => {
+    await request.put('/api/v1/admin/restaurants/1').expect(200)
+  })
+  test('delete restaurant', async () => {
+    await request.put('/api/v1/admin/restaurants/1').expect(200)
+  })
+})
