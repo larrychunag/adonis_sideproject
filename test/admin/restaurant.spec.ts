fa5712f1e0b1d765f8d9aed925d2246ed86e43f7
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-24 14:48:35 +0900

    create database test

--- test/admin/restaurant.spec.ts
+++ test/admin/restaurant.spec.ts
@@ -1,24 +1,34 @@
 import test from 'japa'
-import { RestaurantFactory } from 'database/factories/restaurant'
+import { RestaurantFactory } from '../../database/factories/restaurant'
 import supertest from 'supertest'
+import Database from '@ioc:Adonis/Lucid/Database'
 
-const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}/api/v1`
 const request = supertest(BASE_URL)
 
-test.group('Restaurant', () => {
+test.group('Restaurant', (group) => {
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+  test('create restaurant data', async () => {
+    await RestaurantFactory.create()
+  })
   test('restaurant get all', async () => {
-    await request.get('/api/v1/admin/restaurants').expect(200)
+    await request.get('/admin/restaurants').expect(200)
   })
   test('restaurant get single restaurant', async () => {
-    await request.get('/api/v1/admin/restaurants/1').expect(200)
+    await request.get('/admin/restaurants/1').expect(200)
   })
   test('create restaurant', async () => {
-    await request.post('/api/v1/admin/restaurants').expect(200)
+    await request.post('/admin/restaurants').expect(200)
   })
   test('update restaurant', async () => {
-    await request.put('/api/v1/admin/restaurants/1').expect(200)
+    await request.put('/admin/restaurants/1').expect(200)
   })
   test('delete restaurant', async () => {
-    await request.delete('/api/v1/admin/restaurants/1').expect(200)
+    await request.delete('/admin/restaurants/1').expect(200)
   })
 })
